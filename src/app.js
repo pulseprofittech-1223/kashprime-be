257b770240d2a96b9b835a5af568fe16ff59924a
Ibrahim Kolabalogun <akinola10302@gmail.com>
2025-11-22 20:16:36 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -33,6 +33,8 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
+      "http://localhost:5175",
+      "http://localhost:5174",
       "http://localhost:3000",
       "http://localhost:9091","http://127.0.0.1:5500",
       process.env.FRONTEND_URL,
